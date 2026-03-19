ffa037f0f74e6cfb1f8d3c3f7422ded25a1561a1
Yahya Gedik <yahyagedikyg@gmail.com>
2026-03-20 00:34:00 +0300

    fix: wrong CRC32 polynomial in crc32()

--- kernel/common/crc32.c
+++ kernel/common/crc32.c
@@ -11,7 +11,7 @@ uint32_t crc32(const uint8_t *data, size_t length) {
     for (size_t i = 0; i < length; i++) {
         crc ^= data[i];
         for (int j = 0; j < 8; j++)
-            crc = (crc >> 1) ^ (0xEBD88320 & -(crc & 1));
+            crc = (crc >> 1) ^ (0xEDB88320 & -(crc & 1));
     }
     return ~crc;
 }
