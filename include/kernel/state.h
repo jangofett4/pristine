35c1c8df3ad7e4ffeae8ecc9947d4495a282d8e6
Yahya Gedik <yahyagedikyg@gmail.com>
2026-04-03 02:06:24 +0300

    feat: add process context switching related files to KernelState
    Previously KernelState didn't hold information about currently running
    process, as there was no processes to hold information of anyway.
    
    As part of userspace setup, this commit adds kernel_stack_top,
    user_stack_top and current_process to KernelState to be later used with
    SWAPGS instruction to swap stacks at syscall time.
    
    Also adds a small verification of these new fields, as these need to be
    at fixed offsets in the KernelState and we don't want padding between
    them, otherwise syscall stub would load garbage into RSP.

--- include/kernel/state.h
+++ include/kernel/state.h
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include <kernel/process.h>
 #include <common/bootinfo.h>
 #include <common/bsfs/bsfs.h>
 #include <common/bsfs/bsfs_ops.h>
@@ -14,6 +15,10 @@
 #include <drivers/video/video.h>
 
 typedef struct {
+    uint64_t       kernel_stack_top;
+    uint64_t       user_stack_rsp;
+    Process        *current_process;
+
     BootInfo       bootinfo;
     
     uint64_t      *gdt;
@@ -28,3 +33,7 @@ typedef struct {
 
     uint64_t      system_memory;
 } KernelState;
+
+_Static_assert(offsetof(KernelState, kernel_stack_top) == 0x00, "kernel_stack_top wrong offset");
+_Static_assert(offsetof(KernelState, user_stack_rsp)   == 0x08, "user_stack_rsp wrong offset");
+_Static_assert(offsetof(KernelState, current_process)  == 0x10, "current_process wrong offset");
