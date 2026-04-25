f132e71c3b29a8e3f6584c655e51591122a4772f
Yahya Gedik <yahyagedikyg@gmail.com>
2026-04-25 05:28:51 +0300

    fix: scheduler quantum fix for scheduler_yield
    When called, scheduler_yield was immediately triggering 0xFF interrupt
    which eventually lands inside the scheduler_loop. This is 'yielding' in
    a sense where a process is relinquishing it's quantum for another
    process, however since we didn't reset the timer counter yet, other
    process actually ends up getting less time than it would have if it
    has gotten scheduled normally.
    
    Fix is to simply reset LAPIC timer on scheduler_yield.

--- include/kernel/scheduler.h
+++ include/kernel/scheduler.h
@@ -5,6 +5,9 @@
 
 #pragma once
 
+#include <kernel/cpu.h>
+#include <kernel/lapic.h>
+#include <kernel/state.h>
 #include <kernel/process.h>
 #include <common/idt64.h>
 
@@ -16,5 +19,7 @@ bool scheduler_add_process(Process *process);
 void scheduler_loop(void);
 
 static inline void scheduler_yield(void) {
+    const CpuState *cpu_state = get_cpu_state();
+    lapic_timer_set_counter(cpu_state->lapic, cpu_state->lapic_timer_speed);
     __asm__ volatile("int $0xFF");
 }
